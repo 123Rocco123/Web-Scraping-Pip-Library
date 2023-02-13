9ac09f73118b3f75ce5bc834786241278624d59f
Rocco Petruccio <petruccio.rocco@gmail.com>
2023-02-13 22:55:53 +0000

    Web Scraping Variables

--- ptrFinance/repetitionFunction.py
+++ ptrFinance/repetitionFunction.py
@@ -16,3 +16,11 @@ def repetitionsFunc(stockName, interval, repetitions):
 
     soup = BeautifulSoup(requests, "html5lib")
 
+    newVolume = float((soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"})).findAll("td")[6].text.replace(",", ""))
+
+    high = soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"}).findAll("td")[2].text
+    low = soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"}).findAll("td")[3].text
+
+    # Current Stock Price update
+    currentPrice = soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"}).findAll("td")[4].text
+
