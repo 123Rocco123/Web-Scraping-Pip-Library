c83e11d78f13d294915ae219280a89687b0caeca
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2023-02-14 12:26:48 +0000

    Function Moved From Old Class

--- ptrFinance/__init__.py
+++ ptrFinance/__init__.py
@@ -10,8 +10,40 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from csv import writer
 
-# Class Imports
-import repetitionFunction
+# Class Variable
+previousVol = 0
+
+# Used for the whileTrueStock
+def repetitionsFunc(stockName, interval, repetitions):
+    global previousVol
+
+    session = HTMLSession()
+    requests = session.get("https://finance.yahoo.com/quote/{stockName}/history?p={stockName}".format(stockName = stockName)).text
+
+    soup = BeautifulSoup(requests, "html5lib")
+
+    newVolume = float((soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"})).findAll("td")[6].text.replace(",", ""))
+
+    high = soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"}).findAll("td")[2].text
+    low = soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"}).findAll("td")[3].text
+
+    # Current Stock Price update
+    currentPrice = soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"}).findAll("td")[4].text
+
+    if previousVol == 0 or previousVol == 0.0:
+        # Current Volume
+        previousVol = float((soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"})).findAll("td")[6].text.replace(",", ""))
+    else:
+        # Current Volume
+        newVolume = float((soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"})).findAll("td")[6].text.replace(",", "")) - previousVol
+
+    with open("{workingDirectory}/ptrFinance/repetitionCSV.csv".format(workingDirectory = os.getcwd()).replace("\\", "/"), "a") as f:
+        writer = csv.writer(f)
+
+        # Stock Price, Volume Per Min
+        writer.writerow([currentPrice, newVolume, high, low])
+
+        previousVol = float((soup.find("tr", {"class" : "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"})).findAll("td")[6].text.replace(",", ""))
 
 # Function used to gather main information
 def stockInformation(url, url1, url2):
