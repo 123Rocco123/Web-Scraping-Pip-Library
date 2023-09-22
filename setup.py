16b6c4f53a37b7d5afefd64c93bcf57fde558664
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2023-09-22 04:30:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
   name='ptrFinance',
-  version='0.1.16',
+  version='0.1.18',
   description='Financial Web Scraping Library',
   long_description= open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='',
